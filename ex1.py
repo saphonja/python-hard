4bd84e4b3c5baf818ad38779e9ebc89a7e1aa1c9
Hanna Zaremba <hanna.zaremba@gmail.com>
2017-02-19 17:15:13 +0300

    for 19.02

--- ex1.py
+++ ex1.py
@@ -1,7 +1,19 @@
-print "Hello World!"
-print "Hello Again!"
-print "I like typing this."
-print "This is fun."
-print "Yay! Printing."
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d centimeters tall." % (height * 2.54)
+print "He's %d pounds heavy." % weight
+print "He's %d kgs heavy." % (weight * 0.453)
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+#this line is tricky, try to get is exactly right
+print "If I add %d, %d, and %d I get %d" % (age, height, weight, age + height + weight)
